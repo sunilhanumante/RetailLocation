cd4358b834e03b461aaf63519d8dd443db41a877
iam-Sunil <iamsmh7@gmail.com>
2023-07-23 10:13:53 +0530

    This is the first project of RetailLocation Service

---
+++ src/main/java/com/BikkadIT/RetaiLocation/RetaiLocationApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.RetaiLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetaiLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetaiLocationApplication.class, args);
+	}
+
+}
